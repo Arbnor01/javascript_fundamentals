9f36ed0eecb332608696781ad86000ced84dc057
Arbnor <arbnor-sfarca@hotmail.com>
2023-06-12 14:08:57 +0100

    Defined the function sayHello, which returns the string 'Hello'.

---
+++ exercise_4.js
@@ -0,0 +1,14 @@
+// Exercise
+
+// Define (in a file sayHello.js) the function sayHello, which returns the string 'Hello'.
+
+// Require and call that function in node - you should get the following result:
+
+// > sayHello();
+// 'Hello'
+
+const sayHello = () => {
+    return 'Hello';
+  }
+
+  console.log (sayHello());
