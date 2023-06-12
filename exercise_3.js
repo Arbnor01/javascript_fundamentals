18a5e9db49e6efd2006fd4eae33a2dcc29d861ab
Arbnor <arbnor-sfarca@hotmail.com>
2023-06-12 13:31:15 +0100

    Add variables and console.log expressions for addition and multiplication

---
+++ exercise_3.js
@@ -0,0 +1,18 @@
+// Exercise
+
+// In the node REPL, declare two variables — a with value 4 and b with value 6.
+// Using console.log, print the result of:
+// a + b (should print 10)
+// a * b (should print 24)
+
+
+> const a = 4;
+undefined
+>  const b = 6;
+undefined
+> console.log(a + b);
+10
+undefined
+> console.log(a * b);
+24
+undefined  
