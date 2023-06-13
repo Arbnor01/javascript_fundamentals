f68b88e018e564853d98e9ac1c30946aeaaef88e
Arbnor <arbnor-sfarca@hotmail.com>
2023-06-13 11:39:22 +0100

    Added transform function for given string

--- 7_functions_as_values/exercise.js
+++ 7_functions_as_values/exercise.js
@@ -2,7 +2,11 @@ const lowercaseMessage = (message) => {
     return message.toLowerCase();
   }
 
+  const transform = (message, transformFunction) => {
+    return transformFunction(message);
+}
+
   const inputString = 'MY NAME IS ARBNOR';
-  const result = lowercaseMessage(inputString);
+  const result = transform(inputString, lowercaseMessage);
   
   console.log(result);
