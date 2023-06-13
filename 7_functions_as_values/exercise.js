0945fa3fd4f29cf967ceeeaf0a2be7db6d0ad736
Arbnor <arbnor-sfarca@hotmail.com>
2023-06-13 11:25:34 +0100

    Addded fucntion that returns lowercase version of a given string

---
+++ 7_functions_as_values/exercise.js
@@ -0,0 +1,8 @@
+const lowercaseMessage = (message) => {
+    return message.toLowerCase();
+  }
+
+  const inputString = 'MY NAME IS ARBNOR';
+  const result = lowercaseMessage(inputString);
+  
+  console.log(result);
